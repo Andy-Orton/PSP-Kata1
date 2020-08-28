f5b5c4304e330adea1f008bb732b082545a73bbb
Andy Orton <andyorton13@gmail.com>
2020-08-28 14:16:35 -0600

    Add project files.

---
+++ Kata1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kata1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public void stringToMatrix(List<string> lines)
+        {
+            Boolean[,,] characters = new Boolean[3, 3, 9];
+
+            for (int entry = 0; entry < 27; ++entry)
+            {
+                for (int line = 0; line < 3; ++line)
+                {
+                    for (int character = 0; character < 3; ++character)
+                    {
+                        characters.
+                    }
+                }
+
+            }
+        }
+    }
+
+}
+            
