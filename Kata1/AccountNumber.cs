3e97c6aa3ec2b551e787697bc057694368c4a275
Andy Orton <andyorton13@gmail.com>
2020-08-31 13:04:03 -0600

    robots are big spook

---
+++ Kata1/AccountNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata1
+{
+    public class AccountNumber
+    {
+        //public static readonly char[3, 3, 10] digitCodes
+
+        public char[,,] digits = new char[3, 3, 9];
+        public int[] convertedDigits = new int[9];
+
+        public void convert()
+        {
+
+        }
+
+        public AccountNumber(char[,,] digits)
+        {
+            this.digits = digits;
+            convert();
+        }
+
+        public Boolean ValidChecksum()
+        {
+            return true;
+        }
+    }
+}
